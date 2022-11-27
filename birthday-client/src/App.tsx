f94b4b6ffdba2f0dbaf9b9c7be1121a078415673
Michael-Gubb <112699394+Michael-Gubb@users.noreply.github.com>
2022-11-27 17:59:39 +1300

    Refactor to use component/util

--- birthday-client/src/App.tsx
+++ birthday-client/src/App.tsx
@@ -1,17 +1,6 @@
-import { FunctionComponent } from "react";
-import { Person } from "./types/types";
+import { DateDisplay } from "./components/DateDisplay";
 import people from "./data/testPersons.json";
 import "./App.css";
-
-const DateDisplay: FunctionComponent<Person> = (person) => {
-  return (
-    <div>
-      <h2>{person.name}</h2>
-      <p>CURRENTLY DISPLAYING BIRTHDATE: {person.birthdate}</p>
-      <p>TODO: DISPLAY BIRTHDAY AND DAYS TO BIRTHDAY</p>
-    </div>
-  );
-};
 
 function App() {
   return (
--- birthday-client/src/components/DateDisplay.tsx
+++ birthday-client/src/components/DateDisplay.tsx
@@ -1,11 +1,13 @@
 import { FunctionComponent } from "react";
+import { findBirthday } from "../utils/birthdayCalculator";
 import { Person } from "../types/types";
 
 const DateDisplay: FunctionComponent<Person> = (person) => {
+  const { birthday } = findBirthday(person);
   return (
     <div>
       <h2>{person.name}</h2>
-      <p>CURRENTLY DISPLAYING BIRTHDATE: {person.birthdate}</p>
+      <p>CURRENTLY DISPLAYING BIRTHDATE: {birthday}</p>
       <p>TODO: DISPLAY BIRTHDAY AND DAYS TO BIRTHDAY</p>
     </div>
   );

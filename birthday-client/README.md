837597fceeaa371d807e3de37563bbe04660b47b
Michael-Gubb <112699394+Michael-Gubb@users.noreply.github.com>
2022-11-29 10:22:45 +1300

    Page now loads and displays list of people (#13)
    * Update README.md
    
    * Update to list and convert sourcefile to ts
    
    * Display days to birhday and improve text
    
    * Change list elements to li tag
    
    * More closely emulate fetching

--- birthday-client/README.md
+++ birthday-client/README.md
@@ -2,4 +2,4 @@
 
 Created in Vite with Typescript
 
-Birthday cake svg ![<https://www.svgrepo.com/svg/259042/birthday-cake>](/birthday-client/public/birthday-cake-svgrepo-com.svg =25x) sourced from <https://www.svgrepo.com/svg/259042/birthday-cake>
+Birthday cake svg <img src="/birthday-client/public/birthday-cake-svgrepo-com.svg" width="25"> sourced from <https://www.svgrepo.com/svg/259042/birthday-cake>
--- birthday-client/src/components/DateDisplay.tsx
+++ birthday-client/src/components/DateDisplay.tsx
@@ -1,16 +1,69 @@
-import { FunctionComponent } from "react";
+import { FunctionComponent, useState, useEffect } from "react";
 import { findBirthday } from "../utils/birthdayCalculator";
 import { Person } from "../types/types";
+import peopleFromFile from "../data/testPersons";
 
-const DateDisplay: FunctionComponent<Person> = (person) => {
-  const { birthday } = findBirthday(person, new Date());
+interface DateDisplayElementProps {
+  person: Person;
+}
+
+const DateDisplayElement: FunctionComponent<DateDisplayElementProps> = ({
+  person,
+}) => {
+  const { birthday, daysToBirthday } = findBirthday(person, new Date());
   return (
-    <div>
+    <li>
       <h2>{person.name}</h2>
-      <p>CURRENTLY DISPLAYING BIRTHDATE: {birthday}</p>
-      <p>TODO: DISPLAY BIRTHDAY AND DAYS TO BIRTHDAY</p>
-    </div>
+      <p>Birthday: {birthday}</p>
+      <p>Days to birthday: {daysToBirthday} </p>
+    </li>
   );
+};
+
+interface DateDisplayListProps {
+  people: Person[];
+}
+
+const DateDisplayList: FunctionComponent<DateDisplayListProps> = ({
+  people,
+}) => {
+  return (
+    <ul>
+      {people.map((person) => {
+        return <DateDisplayElement key={person.id} person={person} />;
+      })}
+    </ul>
+  );
+};
+
+const DateDisplay: FunctionComponent = () => {
+  //Using state only to emulate fetching data from server
+  const [people, setPeople] = useState<Person[]>([]);
+
+  useEffect(() => {
+    let ignore = false;
+    if (!ignore) {
+      // put fetch logic here
+      setPeople(peopleFromFile);
+    }
+    return () => {
+      ignore = true;
+    };
+  }, []);
+
+  if (people.length === 0) {
+    return (
+      <>
+        <p>No people found!</p>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <DateDisplayList people={people} />
+      </>
+    );
+  }
 };
 
 export { DateDisplay };
